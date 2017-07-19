199128681ee502f5e2566b6cfd34a707968e293f
ubuntu1234 <ryu88326@yahoo.co.jp>
2017-07-19 09:36:02 +0000

    run test

--- scripts/wall_stop_accel.py
+++ scripts/wall_stop_accel.py
@@ -21,12 +21,11 @@ class WallStopAccel():
         accel = 0.02
         data.linear.x = 0.0
         while not rospy.is_shutdown():
-            data.linear.x = accel
+            data.linear.x += accel
     
             if self.sensor_values.sum_all >= 50: data.linear.x = 0.0
             elif data.linear.x <= 0.2:           data.linear.x = 0.2
             elif data.linear.x >= 0.8:           data.linear.x = 0.8
-
 
             self.cmd_vel.publish(data)
             rate.sleep()
