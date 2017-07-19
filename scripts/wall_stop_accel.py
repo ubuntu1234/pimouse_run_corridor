bf9a5dbf377be22f88f0c3ec5379feb4db6de516
ubuntu1234 <ryu88326@yahoo.co.jp>
2017-07-19 06:52:08 +0000

    Run

--- scripts/wall_stop_accel.py
+++ scripts/wall_stop_accel.py
@@ -32,9 +32,9 @@ class WallStop():
             rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('wall_stop')
+    rospy.init_node('wall_stop_accel)')
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
     rospy.ServiceProxy('/motor_on',Trigger).call()
-    WallStop().run()
+    WallStopAccel().run()
