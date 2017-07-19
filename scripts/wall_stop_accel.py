8ca994c1af1b0cc00e678c7b88afc43471fbb425
ubuntu1234 <ryu88326@yahoo.co.jp>
2017-07-19 07:03:02 +0000

    Run

--- scripts/wall_stop_accel.py
+++ scripts/wall_stop_accel.py
@@ -4,7 +4,7 @@ from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
-class WallStop():
+class WallStopAccel():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 
